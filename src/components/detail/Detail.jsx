5f6c211d978332a862a05ea003b654fd51653001
kang sola <sl0778@naver.com>
2025-02-04 21:24:24 +0900

    feat :댓글다는 부분, 투표결과 코드 추가 작성 및 수정

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,39 +2,61 @@ import React, { useState } from "react";
 
 function Detail() {
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
 
   const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
+    setComment(e.target.value);
   };
-
-
+  const handleCommentSubmit = () => {
+    if(comment.length < 1) return;
+  };
+  
   return (
-    <div id="detail-container">
-      <div id="selected-category">
-        <h1>고백방법</h1>
-      </div>
-      <div id="result">
-        <div>
-        </div>
-      </div>
-      <div id="categories">
-        <ul>
-          <li>고백방법</li>
-          <li>연상/연하</li>
-          <li>첫 데이트</li>
-          <li>기념일</li>
-          <li>만남 횟수</li>
-          <li>B</li>
-        </ul>
-      </div>
-    </div>
-    <div id="comment-container">
+  <div className="comment-container">
       <input
         type="text"
         placeholder="댓글 달기"
         value={comment}
         onChange={handleCommentChange}
         />
+        <button onClick={handleCommentSubmit}></button>
     </div>
-  )
-};
+  );
+}
+
+function Vote ({ vote_1, vote_2 }) {
+  const VotePercentage = (vote_1 + vote_2) => {
+    const totalVotes = vote_1 + vote_2;
+    const vote_1Percentage = (vote_1 / totalVotes) * 100;
+    const vote_2Percentage = (vote_2 / totalVotes) * 100;
+  };
+
+  return (
+    <div className="detail-container">
+      <div className="selected-category">
+        <h1>고백방법</h1>
+      </div>
+      <div className="result">
+        <div>
+          <span>{/*항목명*/}</span>
+          <span>{vote_1Percentage}%</span>
+          <span>{vote_1}표</span>
+        </div>
+        <div>
+          <span>{/*항목명*/}</span>
+          <span>{vote_2Percentage}%</span>
+          <span>{vote_2}표</span>
+      </div>
+      <div className="categories">
+        <ul>
+          <li>{/*항목명*/}</li>
+          <li>{/*항목명*/}</li>
+          <li>{/*항목명*/}</li>
+          <li>{/*항목명*/}</li>
+          <li>{/*항목명*/}</li>
+          <li>{/*항목명*/}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
