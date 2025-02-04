9f4e1ee0de03e16a69aef1f78e0f2f28efc39179
kang sola <sl0778@naver.com>
2025-02-04 18:38:03 +0900

    feat: 결과창 및 댓글창 구역 나눈 코드 일부 작성해봄

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,3 +1,40 @@
-export const Detail = () => {
-  return <div>상세 페이지</div>;
+import React, { useState } from "react";
+
+function Detail() {
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+
+  return (
+    <div id="detail-container">
+      <div id="selected-category">
+        <h1>고백방법</h1>
+      </div>
+      <div id="result">
+        <div>
+        </div>
+      </div>
+      <div id="categories">
+        <ul>
+          <li>고백방법</li>
+          <li>연상/연하</li>
+          <li>첫 데이트</li>
+          <li>기념일</li>
+          <li>만남 횟수</li>
+          <li>B</li>
+        </ul>
+      </div>
+    </div>
+    <div id="comment-container">
+      <input
+        type="text"
+        placeholder="댓글 달기"
+        value={comment}
+        onChange={handleCommentChange}
+        />
+    </div>
+  )
 };
