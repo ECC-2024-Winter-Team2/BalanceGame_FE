358e4c85f53eb9de3fd67dba441caa69c647f6d6
ruabee <annyrhdtjd@naver.com>
2025-02-15 06:25:35 +0900

    chore: 4가지 카테고리 배열 데이터 작성완료

--- src/components/game/game.const.js
+++ src/components/game/game.const.js
@@ -1,5 +1,5 @@
 export const GAME_DATA = {
-  love: [
+   love: [
     {
       orderIndex: 1,
       questionText: "나이",
@@ -50,8 +50,162 @@ export const GAME_DATA = {
       questionText: "성격",
       choices: ["외향적", "내향적"],
     }
-    // TODO: 10개
-  ]
-  /*,
-  life: {},*/
+  ],
+  food: [
+    {
+      orderIndex: 1,
+      questionText: "김밥",
+      choices: ["참치김밥", "계란김밥"],
+    },
+    {
+      orderIndex: 2,
+      questionText: "치킨",
+      choices: ["양념치킨", "후라이드치킨"],
+    },
+    {
+      orderIndex: 3,
+      questionText: "라면",
+      choices: ["볶음라면", "국물라면"],
+    },
+    {
+      orderIndex: 4,
+      questionText: "붕어빵",
+      choices: ["팥붕", "슈붕"],
+    },
+    {
+      orderIndex: 5,
+      questionText: "아이스크림",
+      choices: ["민초", "반민초"],
+    },
+    {
+      orderIndex: 6,
+      questionText: "찌개",
+      choices: ["김치찌개", "된장찌개"],
+    },
+    {
+      orderIndex: 7,
+      questionText: "냉면",
+      choices: ["물냉", "비냉"],
+    },
+    {
+      orderIndex: 8,
+      questionText: "중식",
+      choices: ["짜장면", "짬뽕"],
+    },
+    {
+      orderIndex: 9,
+      questionText: "음료수",
+      choices: ["버블티", "커피"],
+    },
+    {
+      orderIndex: 10,
+      questionText: "밥",
+      choices: ["비빔밥", "볶음밥"],
+    }
+  ],
+  horrible: [
+    {
+      orderIndex: 1,
+      questionText: "카레",
+      choices: ["똥\n맛\n카레", "카레\n맛\n똥"],
+    },
+    {
+      orderIndex: 2,
+      questionText: "머리",
+      choices: ["5일\n동안\n머리\n안감기", "5일\n동안\n양치\n안하기"],
+    },
+    {
+      orderIndex: 3,
+      questionText: "항문에\n이빨이\n자라면",
+      choices: ["치과를\n간다", "비뇨기괴를\n간다"],
+    },
+    {
+      orderIndex: 4,
+      questionText: "가족이\n바퀴벌레로\n변하면",
+      choices: ["잡는다", "키운다"],
+    },
+    {
+      orderIndex: 5,
+      questionText: "결혼상대",
+      choices: ["100억부자\n유병재", "백수\n차은우"],
+    },
+    {
+      orderIndex: 6,
+      questionText: "등산",
+      choices: ["제로투\n댄스\n추면서\n등산", "트월킹\n하면서\n등산"],
+    },
+    {
+      orderIndex: 7,
+      questionText: "털",
+      choices: ["모든\n부위\n털\n안나기", "모든\n부위\n털\n수북하게\n나기"],
+    },
+    {
+      orderIndex: 8,
+      questionText: "노출",
+      choices: ["사생활\n노출", "노출로\n생활"],
+    },
+    {
+      orderIndex: 9,
+      questionText: "최애",
+      choices: ["최애\n씻겨주기", "최애한테\n씻김\n당하기"],
+    },
+    {
+      orderIndex: 10,
+      questionText: "똥",
+      choices: ["밥\n먹는\n곳에서\n똥\n싸기", "똥\n싸는\n곳에서\n밥\n먹기"],
+    }
+  ],
+  life: [
+    {
+      orderIndex: 1,
+      questionText: "삶의\n태도",
+      choices: ["YOLO", "계획적\n삶"],
+    },
+    {
+      orderIndex: 2,
+      questionText: "친구\n관계",
+      choices: ["소수정예", "다다익선"],
+    },
+    {
+      orderIndex: 3,
+      questionText: "성공",
+      choices: ["돈", "행복"],
+    },
+    {
+      orderIndex: 4,
+      questionText: "휴식",
+      choices: ["집", "친구"],
+    },
+    {
+      orderIndex: 5,
+      questionText: "강의\n스타일",
+      choices: ["강의력\n최고\t장미칼\n학점", "강의력\n0\t학점\n퍼줌"],
+    },
+    {
+      orderIndex: 6,
+      questionText: "여행",
+      choices: ["도시", "시골"],
+    },
+    {
+      orderIndex: 7,
+      questionText: "생활\n패턴",
+      choices: ["아침형", "밤형"],
+    },
+    {
+      orderIndex: 8,
+      questionText: "영화\n시청",
+      choices: ["집에서", "영화관에서"],
+    },
+    {
+      orderIndex: 9,
+      questionText: "감성",
+      choices: ["디지털", "아날로그"],
+    },
+    {
+      orderIndex: 10,
+      questionText: "돈",
+      choices: ["FLEX", "저축"],
+    }
+  ],
+
 };
